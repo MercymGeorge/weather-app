4ad42b0683675d507e32ea26586aa6720aebde2c
Mercy Morounkeji George <mercygeorge79@gmail.com>
2023-08-04 18:00:38 +0100

    api

--- src/app.js
+++ src/app.js
@@ -13,7 +13,8 @@ function formatDate(timestamp){
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast(){
+function displayForecast(response){
+    console.log(response.data.daily);
     let forecastElement = document.querySelector("#forecast");
 
 let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -44,6 +45,13 @@ forecastElement.innerHTML = forecastHTML;
 
 }
 
+function getForecast(coordinates){
+    console.log(coordinates);
+    let apiKey = "317c00affdfe3076336b11eaf996b675";
+    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat={coordinates.lat}&lon={coordinates.lon}&exclude={part}&appid={apikey}&unit=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response){
    
     let temperatureElement = document.querySelector("#temperature");
@@ -67,6 +75,7 @@ function displayTemperature(response){
     iconElement.setAttribute("src",  `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`) ;
     iconElement.setAttribute("alt", response.data.weather[0].description);
 
+    getForecast(respnse.data.coord);
 }
 function search(city){
     let apiKey = "317c00affdfe3076336b11eaf996b675";
